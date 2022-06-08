d05c092362e9dd6bda53fb8469726aefd6bad30b
ihatedebug <jongh97@snu.ac.kr>
2022-06-08 19:09:56 +0900

    abs

--- hsd22_project/src/fpga_api_on_cpu.cpp
+++ hsd22_project/src/fpga_api_on_cpu.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 #define min(x,y) (((x)<(y))?(x):(y))
+#define ABS(s) ((s)>0 ? (s) : -(s))
 
 FPGA::FPGA(off_t data_addr, off_t output_addr, int m_size, int v_size)
 {
@@ -166,7 +167,7 @@ void FPGA::largeMM(const float *weight_mat, const float *input_mat, float *outpu
     for (int j = 0; j < num_output; j++){
         // 1) Compute Non-zero data & indices
         for(int i = 0; i< GROUP_NUM ; i++){
-            float *test_block = {abs(weight_mat[j*num_input + i*ELEM_NUM]), abs(weight_mat[j*num_input + i*ELEM_NUM + 1]), abs(weight_mat[j*num_input + i*ELEM_NUM + 2]), abs(weight_mat[j*num_input + i*ELEM_NUM + 3])};
+            float *test_block = {ABS(weight_mat[j*num_input + i*ELEM_NUM]), ABS(weight_mat[j*num_input + i*ELEM_NUM + 1]), ABS(weight_mat[j*num_input + i*ELEM_NUM + 2]), ABS(weight_mat[j*num_input + i*ELEM_NUM + 3])};
             int min1_index = 0;
             int min2_index = 1;
             if(test_block[2]<test_block[min1_index])
